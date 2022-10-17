0dfdd05db83a2b3b29a55a5c1a6abaedeaa1ef97
royrohit15 <115971882+royrohit15@users.noreply.github.com>
2022-10-17 11:31:58 +0530

    Create palindrome.cpp

---
+++ palindrome.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+bool checkpalindrome(char a[],int n){
+    int s=0;
+    int e=n-1;
+    while(s<=e){
+        if(a[s] !=a[e]){
+            return 0; 
+        }
+        else{
+            s++;
+            e--;
+        } 
+    }
+    return 1;
+}
+
+void reverse(char name[],int n){
+    int s=0;
+    int e=n-1;
+    while(s<e){
+        swap(name[s++],name[e--]);
+    }
+}
+int getLength(char name[]){
+    int count=0;
+    for( int i=0; name[i] !='\0'; i++ ){
+        count++;
+    }
+    return count;
+}
+
+int main()
+{
+     char name[20];
+    cout<<"Enter your name "<<endl;
+    cin>> name;
+    cout<<"your name is ";
+    cout<<name<<endl;
+    int len =getLength(name);
+    cout<<"Lenghth:"<< len <<endl;
+    reverse(name,len);
+    cout<<"your name is ";
+    cout<<name<<endl;
+    
+   cout<<"palindrome or Not" <<checkpalindrome(name,len)<<endl;
+    
+    return 0;
+}
